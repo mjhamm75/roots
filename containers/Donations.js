36db94d63f9e5e6bccca76ca7e9f6f9104439c3c
Jason Hamm <jasonhamm.me@gmail.com>
2015-11-19 06:15:46 -0700

    Add loader for file and img

--- containers/Donations.js
+++ containers/Donations.js
@@ -3,9 +3,12 @@ import { connect } from 'react-redux';
 
 class Donations extends Component {
 	render() {
+		var woody = "#woody";
+		var src = require("./../imgs/woody.jpg");
 		return (
 			<div>
 				<h1>Donations</h1>
+				<img src={src} alt="Missing" useMap={woody}/>
 			</div>
 		)
 	}
--- webpack.config.js
+++ webpack.config.js
@@ -18,12 +18,21 @@ module.exports = {
     new webpack.NoErrorsPlugin()
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      exclude: /node_modules/,
-      include: __dirname
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        exclude: /node_modules/,
+        include: __dirname
+      },
+      {
+        test: /.*\.(gif|png|jpe?g|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}'
+        ]
+      }
+    ]
   }
 };
 
