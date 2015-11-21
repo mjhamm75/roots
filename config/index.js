a6f592bbec42196963386c01f1075e624e8b7370
Jason Hamm <jasonhamm.me@gmail.com>
2015-11-21 15:27:35 -0700

    Remove unused code

--- config/index.js
+++ config/index.js
@@ -1,8 +1,1 @@
 export const PORT = process.env.PORT || 8888;
-
-export function knexInit() {
-	return require('knex')({
-	    client: 'pg',
-	    connection: 'postgres://killem:killem@localhost/killem'
-	});	
-}
