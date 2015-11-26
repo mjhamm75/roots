9615868943fcef1693f210fbfefd4111eb1ef894
Jason Hamm <jasonhamm.me@gmail.com>
2015-11-26 07:42:42 -0700

    Remove more unused code

--- highlight.react.js
+++ highlight.react.js
@@ -63,26 +63,7 @@ export function highlightArea(e) {
 	var area_options = options_from_area(area, options);
 	if(!area_options.neverOn && !area_options.alwaysOn) {
 		var shape = shape_from_area(area);
-		add_shape_to(canvas, shape[0], shape[1], area_options, "highlighted");
-		if(area_options.groupBy) {
-			var areas;
-			// two ways groupBy might work; attribute and selector
-			if(/^[a-zA-Z][\-a-zA-Z]+$/.test(area_options.groupBy)) {
-				areas = map.find('area['+area_options.groupBy+'="'+$(area).attr(area_options.groupBy)+'"]');
-			} else {
-				areas = map.find(area_options.groupBy);
-			}
-			var first = area;
-			areas.each(function() {
-				if(image != first) {
-					var subarea_options = options_from_area(image, options);
-					if(!subarea_options.neverOn && !subarea_options.alwaysOn) {
-						var shape = shape_from_area(image);
-						add_shape_to(canvas, shape[0], shape[1], subarea_options, "highlighted");
-					}
-				}
-			});
-		}
+		add_shape_to(canvas, shape[0], shape[1], area_options, "highlighted");		
 	}
 }
 
